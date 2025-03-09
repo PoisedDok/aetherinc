20fca88fcd20d8a039280d7a8ec1af1e2259ed94
Krish Dokania <2767195d@student.gla.ac,uk>
2025-03-09 14:17:17 +0000

    Dockerized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-"use client"
 
+/* eslint-disable react/no-unescaped-entities */
+"use client"
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useState } from 'react';
